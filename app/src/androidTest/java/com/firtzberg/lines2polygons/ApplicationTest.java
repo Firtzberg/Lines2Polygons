ae21127b7095236086f6bd7a031c0f03384f435e
Firtzberg <hrvoje_jukic@hotmail.com>
2017-10-22 13:59:15 +0200

    Initial commit

---
+++ app/src/androidTest/java/com/firtzberg/lines2polygons/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.firtzberg.lines2polygons;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/com/firtzberg/lines2polygons/LinesActivity.java
@@ -0,0 +1,13 @@
+package com.firtzberg.lines2polygons;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LinesActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lines);
+    }
+}
